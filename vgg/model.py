778898170696da9d72a01dce5fd755e73e99849a
jinwonkim93 <jinwonkim93@github.com>
2023-06-17 22:56:23 +0900

    Same state_dict with torchvision

--- vgg/model.py
+++ vgg/model.py
@@ -4,89 +4,80 @@ class VGG16(torch.nn.Module):
     def __init__(self, n_classes=10):
         super().__init__()
         
-        self.relu = torch.nn.ReLU(inplace=False)
-        self.maxpool = torch.nn.MaxPool2d(kernel_size=2, stride=2)
-        self.softmax = torch.nn.Softmax(1)
+        relu = torch.nn.ReLU(inplace=True)
+        maxpool = torch.nn.MaxPool2d(kernel_size=2, stride=2)
+        dropout = torch.nn.Dropout(p=0.5, inplace=False)
 
-        self.conv1 = torch.nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, padding=1)
-        self.conv2 = torch.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1)
-        self.layer1 = torch.nn.Sequential(
-            self.conv1,
-            self.relu,
-            self.conv2,
-            self.relu,
-            self.maxpool
+        conv1 = torch.nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, padding=1)
+        conv2 = torch.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1)
+
+        conv3 = torch.nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
+        conv4 = torch.nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1)
+
+        conv5 = torch.nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1)
+        conv6 = torch.nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1)
+        conv7 = torch.nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1)
+
+        conv8 = torch.nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1)
+        conv9 = torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1)
+        conv10 = torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1)
+
+        conv11 = torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1)
+        conv12 = torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1)
+        conv13 = torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1)
+        self.features = torch.nn.Sequential(
+            conv1,
+            relu,
+            conv2,
+            relu,
+            maxpool,
+            conv3,
+            relu,
+            conv4,
+            relu,
+            maxpool,
+            conv5,
+            relu,
+            conv6,
+            relu,
+            conv7,
+            relu,
+            maxpool,
+            conv8,
+            relu,
+            conv9,
+            relu,
+            conv10,
+            relu,
+            maxpool,
+            conv11,
+            relu,
+            conv12,
+            relu,
+            conv13,
+            relu,
+            maxpool,
             )
+        
+        self.avgpool = torch.nn.AdaptiveAvgPool2d(output_size=(7, 7))
 
-        self.conv3 = torch.nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
-        self.conv4 = torch.nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1)
-        self.layer2 = torch.nn.Sequential(
-            self.conv3,
-            self.relu,
-            self.conv4,
-            self.relu,
-            self.maxpool
-            )
-
-        self.conv5 = torch.nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1)
-        self.conv6 = torch.nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1)
-        self.conv7 = torch.nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1)
-        self.layer3 = torch.nn.Sequential(
-            self.conv5,
-            self.relu,
-            self.conv6,
-            self.relu,
-            self.conv7,
-            self.relu,
-            self.maxpool
-            )
-
-        self.conv8 = torch.nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1)
-        self.conv9 = torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1)
-        self.conv10 = torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1)
-        self.layer4 = torch.nn.Sequential(
-            self.conv8,
-            self.relu,
-            self.conv9,
-            self.relu,
-            self.conv10,
-            self.relu,
-            self.maxpool
-            )
-
-        self.conv11 = torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1)
-        self.conv12 = torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1)
-        self.conv13 = torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1)
-        self.layer5 = torch.nn.Sequential(
-            self.conv11,
-            self.relu,
-            self.conv12,
-            self.relu,
-            self.conv13,
-            self.relu,
-            self.maxpool
-            )
-
-        self.flat = torch.nn.Flatten()
-        self.fc1 = torch.nn.LazyLinear(out_features=4096)
-        self.fc2 = torch.nn.Linear(in_features=4096, out_features=4096)
-        self.fc3 = torch.nn.Linear(in_features=4096, out_features=n_classes)
-        self.layer6 = torch.nn.Sequential(
-            self.flat,
-            self.fc1,
-            self.relu,
-            self.fc2,
-            self.relu,
-            self.fc3,
-            self.softmax
+        fc1 = torch.nn.Linear(in_features=25088, out_features=4096)
+        fc2 = torch.nn.Linear(in_features=4096, out_features=4096)
+        fc3 = torch.nn.Linear(in_features=4096, out_features=n_classes)
+        self.classifier = torch.nn.Sequential(
+            fc1,
+            relu,
+            dropout,
+            fc2,
+            relu,
+            dropout,
+            fc3,
             )
     
     def forward(self, x):
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        x = self.layer5(x)
-        x = self.layer6(x)
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
         return x
 
