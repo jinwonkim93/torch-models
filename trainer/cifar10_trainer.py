f6d0498f75c4dc90fc54905da452552f21ba7527
jinwonkim93 <jinwonkim93@github.com>
2023-07-01 10:09:07 +0900

    check acc of cifar10, able to train on mps

--- trainer/cifar10_trainer.py
+++ trainer/cifar10_trainer.py
@@ -4,15 +4,17 @@ import torch
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
 
 
 class Cifar10Trainer:
-    def __init__(self, model):
+    def __init__(self, model, device="cpu"):
         self.model = model
         self.optimizer = torch.optim.SGD(self.model.parameters(), lr=0.01, momentum=0.9)
         self.scheduler = torch.optim.lr_scheduler.ExponentialLR(self.optimizer, gamma=0.9)
         self.loss_fn = torch.nn.CrossEntropyLoss()
-    
+        self.device = torch.device(device)
+
     def train(self, configs):
 
         #TODO##################################
@@ -35,13 +37,27 @@ class Cifar10Trainer:
                                     shuffle=True,
                                     num_workers=num_workers)
 
+
         for epoch in range(max_epoch):
-            for batch_image, batch_target in data_loader:
+
+            train_acc = 0
+            train_loss = 0
+            for idx, data in enumerate(tqdm(data_loader)):
                     self.optimizer.zero_grad()
+                    batch_image, batch_target = data
+                    batch_image = batch_image.to(self.device)
+                    batch_target = batch_target.to(self.device)
                     pred = self.model(batch_image)
                     loss = self.loss_fn(pred, batch_target)     
                     loss.backward()
-                    print(f"Epoch: {epoch}, Loss: {loss.item()}")
+
                     self.optimizer.step()
                     self.scheduler.step()
+
+                    pred = torch.argmax(pred, dim=1)
+
+                    train_loss += loss.item()
+                    train_acc += torch.sum(pred == batch_target)
+            print(f"Epoch: {epoch+1}/{max_epoch}, Loss: {train_loss/len(cifar_data):.3f}, Acc: {train_acc/len(cifar_data):.3f}")
+            
         ############################################
