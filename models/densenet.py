fbf6bbb393df7e8241315ccc82e549652639ffbe
jinwonkim93 <jinwonkim93@github.com>
2023-07-18 00:51:20 +0900

    WIP

--- models/densenet.py
+++ models/densenet.py
@@ -14,11 +14,15 @@ class ConvBlock(nn.Module):
         return self.layer(x)
 
 class DenseBlock(nn.Module):
-    def __init__(self, num_layers, in_channels):
+    """
+    H_l layer
+    """
+    def __init__(self, num_layers, k):
         super().__init__()
         #TODO denseblock 역할이 무엇인지
-        self.module_list = [(ConvBlock(in_channels+(num_layers*(i-1)), in_channels+(num_layers*(i-1)), kernel_size=1),
-                            ConvBlock(in_channels+(num_layers*(i-1)), in_channels+(num_layers*(i-1)), kernel_size=3))
+
+        self.module_list = [(ConvBlock(k+k*(i-1), k+k*(i-1), kernel_size=1, padding=0),
+                            ConvBlock(k+k*(i-1), k+k*(i-1), kernel_size=3))
                             for i in range(1, num_layers+1)
                              ]
         
@@ -27,9 +31,8 @@ class DenseBlock(nn.Module):
         hidden_dims = []
         
         for conv1x1, conv3x3 in self.module_list:
-            
-            if len(hidden_dims) >= 1:
-                x = torch.cat(x+hidden_dims, dim=1)
+            if len(hidden_dims) > 1:
+                x = torch.cat(hidden_dims, dim=1)
             
             x = conv1x1(x)
             x = conv3x3(x)
@@ -45,13 +48,13 @@ class DenseNet(nn.Module):
 
 if __name__ == "__main__":
     input_batch = torch.ones((1, 3, 224, 224))
-    convblock = ConvBlock(3, 64, kernel_size=7, stride=2, padding=3)
+    convblock = ConvBlock(3, 32, kernel_size=7, stride=2, padding=3)
     pool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
     
     # -> 
     # input_hs = torch.ones((4, 512, 112, 112))
 
-    dense_block = DenseBlock(6, 64)
+    dense_block = DenseBlock(6, 32)
 
     h1 = convblock(input_batch)
     print(h1.shape)
@@ -59,4 +62,5 @@ if __name__ == "__main__":
     h2 = pool(h1)
     print(h2.shape)
 
-    print(dense_block.module_list)
+    h3 = dense_block(h2)
+    print(h3.shape)
--- models/vgg.py
+++ models/vgg.py
@@ -74,17 +74,17 @@ class VGG16(torch.nn.Module):
             fc3,
             )
 
-    for m in self.modules():
-        if isinstance(m, nn.Conv2d):
-            nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
-            if m.bias is not None:
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
-        elif isinstance(m, nn.BatchNorm2d):
-            nn.init.constant_(m.weight, 1)
-            nn.init.constant_(m.bias, 0)
-        elif isinstance(m, nn.Linear):
-            nn.init.normal_(m.weight, 0, 0.01)
-            nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
 
     def forward(self, x):
         x = self.features(x)
